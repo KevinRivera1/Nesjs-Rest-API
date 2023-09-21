a30567a31fb45b27752bb3b8a3830ac65e6adc16
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-21 15:05:15 -0500

    Configuracion base de datos postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { InventarioModule } from "./inventario/inventario.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "1234", //* contraseña de la base de datos
+      database: "shopProducts", //* nombre de la base de datos
+      autoLoadEntities: true, //* carga todas las entidades automaticamente
+      synchronize: true, //! Solo para desarrollo en produccion quitar
+    }),
+    InventarioModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
