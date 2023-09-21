8b70a55946b35912ed6fa88a26bf7f0c0573f386
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-21 15:05:44 -0500

    Configuraciones generales de validaciones y Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix("/api/v1"); //*Este es el prefijo que se le agrega a todas las rutas
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //*Solo acepta los datos que estén definidos en el DTO
+      forbidNonWhitelisted: true, //*Si hay un dato que no está definido en el DTO, no lo acepta
+      transform: true, //*Transforma los datos a los tipos de datos que se definen en el DTO
+    })
+  ); //*Validación de datos
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle("Nombre de tu API")
+    .setDescription("Descripción de tu API")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api-docs", app, document);
+
+  await app.listen(3000); //*Puerto en el que se ejecuta la aplicación
 }
 bootstrap();
