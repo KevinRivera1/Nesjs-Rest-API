624e44de34119dfb16e20763c29b5ebd5491c028
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-23 11:15:44 -0500

    Configuracion de la varibles de entorno y la base de datos con typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import { DataSourceconfig } from "./config/data.source";
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { InventarioModule } from "./inventario/inventario.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
+      ...DataSourceconfig,
+      autoLoadEntities: true,
+    }),
+    /* TypeOrmModule.forRoot({
       type: "postgres",
       host: "localhost",
       port: 5432,
@@ -13,7 +23,7 @@ import { InventarioModule } from "./inventario/inventario.module";
       database: "shopProducts", //* nombre de la base de datos
       autoLoadEntities: true, //* carga todas las entidades automaticamente
       synchronize: true, //! Solo para desarrollo en produccion quitar
-    }),
+    }), */
     InventarioModule,
   ],
   controllers: [],
