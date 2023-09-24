e78c54d9682b24cb38efbaeb40d61e5b6b74a45e
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-24 15:16:01 -0500

    formatter code

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService)
-  console.log(configService.get("PORT_SERVER"))
+  const configService = app.get(ConfigService);
+  console.log(configService.get("PORT_SERVER"));
 
   app.use(morgan("dev")); //*Morgan para ver las peticiones que se hacen al servidor
 
